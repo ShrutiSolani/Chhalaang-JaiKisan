285427db4ca2dea90f4d3562e3d543950058c3ea
ShrutiSolani <shrutiusolani@gmail.com>
2022-12-10 17:22:59 +0530

    photo

--- frontend/JaiKisan/App.js
+++ frontend/JaiKisan/App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import GetLocationofLand from './Screens/GetLocationofLand';
+import GetSoilPhoto from './Screens/GetSoilPhoto';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World shrutid!</Text>
       <StatusBar style="auto" />
-      <GetLocationofLand />
+      {/* <GetLocationofLand /> */}
+      <GetSoilPhoto />
     </View>
   );
 }
