e4c12656a73ce3b40c713d4b93792ea718d3a7d9
shreyakulkarni <shreyakulkarni2112@gmail.com>
2022-12-10 17:28:54 +0530

    demo

--- frontend/JaiKisan/App.js
+++ frontend/JaiKisan/App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View ,Image} from 'react-native';
-import Demo from './Screens/Demo.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
       
       <StatusBar style="auto" />
-      <Demo/>
      
     </View>
   );
