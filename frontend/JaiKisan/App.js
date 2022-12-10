b093aacba3c08a3e9c3e41a40477a8ef7b9af965
ShrutiSolani <shrutiusolani@gmail.com>
2022-12-10 15:37:32 +0530

    area

--- frontend/JaiKisan/App.js
+++ frontend/JaiKisan/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import GetLocationofLand from './Screens/GetLocationofLand';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World shrutid!</Text>
       <StatusBar style="auto" />
+      <GetLocationofLand />
     </View>
   );
 }
--- frontend/JaiKisan/Screens/GetLocationofLand.js
+++ frontend/JaiKisan/Screens/GetLocationofLand.js
@@ -9,11 +9,22 @@ export default function GetLocationofLand() {
     const [farmCoordinates, setFarmCoordinates] = useState([]);
 
     const uploadToFirestore = async () => {
-        let area = 98;
-        await setDoc(doc(db, "farmerLand", "land2"), {
+        let area = 0;
+        let x1 = farmCoordinates[0].latitude;
+        let x2 = farmCoordinates[1].latitude;
+        let x3 = farmCoordinates[2].latitude;
+        let x4 = farmCoordinates[3].latitude;
+        let y1 = farmCoordinates[0].longitude;
+        let y2 = farmCoordinates[0].longitude;
+        let y3 = farmCoordinates[0].longitude;
+        let y4 = farmCoordinates[0].longitude;
+
+        area = ((x1 * y2) - (y1 * x2)) + ((x2 * y3) - (y2 * x3)) + ((x3 * y4) - (y3 * x4)) + ((x4 * y1) - (y4 * x1));
+        const docData = {
             coordinates: farmCoordinates,
             area: area
-        });
+        };
+        await setDoc(doc(db, "farmerLand", "one"), docData);
         console.log("uploaded to firestore");
         setFarmCoordinates([]);
     }
@@ -21,13 +32,14 @@ export default function GetLocationofLand() {
 
     const getCurrentLocation = async () => {
         setCurrentLocation(await Location.getCurrentPositionAsync({}));
-        let coordinates = {
-            "latitude": currentLocation.coords.latitude,
-            "longitude": currentLocation.coords.longitude
+        if (currentLocation != null) {
+            let coordinates = {
+                "latitude": currentLocation.coords.latitude,
+                "longitude": currentLocation.coords.longitude
+            }
+            setFarmCoordinates(oldArray => [...oldArray, coordinates]);
         }
-        setFarmCoordinates(oldArray => [...oldArray, coordinates]);
-        console.log("currentLocation : " + JSON.stringify(currentLocation));
-        console.log("farmCoordinates : " + JSON.stringify(farmCoordinates));
+
         if (farmCoordinates.length >= 4) {
             uploadToFirestore();
         }
