80d985c7f889bf35463e463abdb1a98359c3d396
ShrutiSolani <shrutiusolani@gmail.com>
2022-12-10 15:10:12 +0530

    location

--- frontend/JaiKisan/Screens/GetLocationofLand.js
+++ frontend/JaiKisan/Screens/GetLocationofLand.js
@@ -1,18 +1,36 @@
 import React, { useState } from 'react';
 import * as Location from 'expo-location';
 import { Button } from 'react-native';
-
+import db from '../firestore';
+import { doc, setDoc } from "firebase/firestore";
 
 export default function GetLocationofLand() {
-    const navigation = useNavigation();
     const [currentLocation, setCurrentLocation] = useState(null);
     const [farmCoordinates, setFarmCoordinates] = useState([]);
 
+    const uploadToFirestore = async () => {
+        let area = 98;
+        await setDoc(doc(db, "farmerLand", "land2"), {
+            coordinates: farmCoordinates,
+            area: area
+        });
+        console.log("uploaded to firestore");
+        setFarmCoordinates([]);
+    }
+
+
     const getCurrentLocation = async () => {
         setCurrentLocation(await Location.getCurrentPositionAsync({}));
-        setFarmCoordinates(oldArray => [...oldArray, currentLocation]);
+        let coordinates = {
+            "latitude": currentLocation.coords.latitude,
+            "longitude": currentLocation.coords.longitude
+        }
+        setFarmCoordinates(oldArray => [...oldArray, coordinates]);
         console.log("currentLocation : " + JSON.stringify(currentLocation));
         console.log("farmCoordinates : " + JSON.stringify(farmCoordinates));
+        if (farmCoordinates.length >= 4) {
+            uploadToFirestore();
+        }
     }
 
     return (
