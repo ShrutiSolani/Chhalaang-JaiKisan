7f6737aa55ea60acbf51e51e9f8b8b77324aba58
Shruti Solani <68545365+ShrutiSolani@users.noreply.github.com>
2023-04-19 10:15:14 +0530

    Update firestore.js

--- frontend/JaiKisan/firestore.js
+++ frontend/JaiKisan/firestore.js
@@ -3,12 +3,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const configuration = {
-    apiKey: "AIzaSyDd3BycDKmN2O-JH0qzYX9uP649aKMVJyE",
-    authDomain: "chhalaang-hack.firebaseapp.com",
-    projectId: "chhalaang-hack",
-    storageBucket: "chhalaang-hack.appspot.com",
-    messagingSenderId: "1071717653133",
-    appId: "1:1071717653133:web:bf2c157241b9f5a294a0c4"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 }
 
 firebase.initializeApp(configuration)
